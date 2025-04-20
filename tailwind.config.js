2fc4db4c090ef16be0d1045e3cd5bcbe40c46b8b
Mejlisson <sabinamejlisson@gmail.com>
2025-04-20 14:16:29 +0200

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                comicYellow: '#FFD600',
+                comicRed: '#FF1744',
+                comicBlue: '#2979FF',
+            },
+            fontFamily: {
+                comic: ['"Comic Neue"', 'cursive'],
+            },
+        },
+    },
+    plugins: [],
+};
