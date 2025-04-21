4826bfa31deda1b051197b170d86c7810f2a1474
Mejlisson <sabinamejlisson@gmail.com>
2025-04-21 16:10:01 +0200

    add style on seemorebutton

--- src/api/comicVine.tsx
+++ src/api/comicVine.tsx
@@ -1,4 +1,4 @@
-//const API_KEY = "";
+const API_KEY = "";
 const BASE_URL = "https://comicvine.gamespot.com/api";
 
 export async function fetchMainComic() {
--- src/components/homepage/SeeMoreButton.tsx
+++ src/components/homepage/SeeMoreButton.tsx
@@ -1,6 +1,6 @@
 export default function SeeMoreButton() {
     return (
-        <button className="bg-yellow-300 text-black text-s">
+        <button className="absolute top-0 right-0 bg-yellow-300 text-black text-sm px-2 py-1 rounded-bl-xl shadow-md hover:bg-yellow-400 z-10">
             See more...
         </button>
     );
