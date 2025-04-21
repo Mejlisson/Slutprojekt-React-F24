eecde79f18b2feadc4b5013bbd3f1a6513044775
Mejlisson <sabinamejlisson@gmail.com>
2025-04-21 16:17:46 +0200

    add hover effect on seemoreButton

--- src/components/homepage/SeeMoreButton.tsx
+++ src/components/homepage/SeeMoreButton.tsx
@@ -1,6 +1,7 @@
 export default function SeeMoreButton() {
     return (
-        <button className="absolute top-0 right-0 bg-yellow-300 text-black text-sm px-2 py-1 rounded-bl-xl shadow-md hover:bg-yellow-400 z-10">
+        <button className="absolute top-0 right-0 bg-yellow-300 text-black text-s px-2 py-1 rounded-bl-xl shadow-md z-10
+                 transition-transform duration-200 ease-in-out hover:scale-115 hover:bg-yellow-500">
             See more...
         </button>
     );
---
+++ src/components/homepage/UpcomingEvents.tsx
@@ -0,0 +1,12 @@
+import SectionBlock from "../layout/SectionBlock";
+import SeeMoreButton from "./SeeMoreButton";
+
+export default function UpcommingEvents() {
+    return (
+        <SectionBlock className="mt-4">
+            <SeeMoreButton />
+            <h2 className="text-xl font-bold mb-1">Popular Movies</h2>
+            <div className="h-32 bg-white flex items-center justify-center rounded">content here</div>
+        </SectionBlock>
+    );
+}
