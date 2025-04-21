d817ba2f5b9f5421685c524ba672b71954c8241e
Mejlisson <sabinamejlisson@gmail.com>
2025-04-21 12:34:10 +0200

    Add SeeMoreButton component

---
+++ src/components/SeeMoreButton.tsx
@@ -0,0 +1,7 @@
+export default function SeeMoreButton() {
+    return (
+        <button className="bg-yellow-300 text-xs px-2 py-1 rounded-full border border-black shadow-lg hover:bg-yellow-400 transition absolute top-2 right-2 z-10">
+            See more...
+        </button>
+    );
+}
