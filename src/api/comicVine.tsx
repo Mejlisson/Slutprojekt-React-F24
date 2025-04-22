ea98cc56c0729076d09d67e1fad7b0d1f5521de6
Mejlisson <sabinamejlisson@gmail.com>
2025-04-22 12:35:22 +0200

    improved API fetch

--- src/api/comicVine.tsx
+++ src/api/comicVine.tsx
@@ -2,11 +2,12 @@ const API_KEY = "";
 const BASE_URL = "https://comicvine.gamespot.com/api";
 
 export async function fetchMainComic() {
-    const url = `${BASE_URL}/search/?api_key=${API_KEY}&format=json&query=wolverine&resources=volume&limit=10&sort=start_year:desc`;
+    const url = `/api/search/?api_key=${API_KEY}&format=json&query=wolverine&resources=volume&limit=10&sort=start_year:desc`;
 
     const response = await fetch(url);
 
     const data = await response.json();
+    console.log(data);
 
     const randomIndex = Math.floor(Math.random() * data.results.length);
     return data.results[randomIndex];
