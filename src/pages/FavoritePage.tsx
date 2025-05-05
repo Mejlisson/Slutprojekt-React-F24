e6ac0543437623cb11010e22967cddcc03d85741
Mejlisson <sabinamejlisson@gmail.com>
2025-05-05 21:30:06 +0200

    Add count to FavoritePage and average rating display to RatingPage

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -5,12 +5,20 @@ import { useFavorite } from "../context/FavoriteContext";
 function FavoritePage() {
     const { state, dispatch } = useFavorite();
     const favorites = state.favorites;
+    const totalFavorites = favorites.length;
+
 
     return (
         <div className="min-h-screen bg-comic-pattern p-4 text-left mt-35 ml-18">
-            <h1 className="text-4xl font-bold text-red-600 bg-yellow-300 border-4 border-black px-4 py-2 inline-block mb-6">
-                YOUR FAVORITES
-            </h1>
+            <div className="flex justify-between items-center mb-6">
+                <h1 className="text-4xl font-bold text-red-600 bg-yellow-300 border-4 border-black px-4 py-2">
+                    YOUR FAVORITES
+                </h1>
+                <span className="text-md text-black mr-18 font-semibold bg-pink-500 border-4 border-black shadow-[4px_4px_0px_black] p-2 text-center [backface-visibility:hidden] transition duration-300 ease-in-out hover:scale-110 hover:-translate-y-1">
+                    Total Cards: {totalFavorites}
+                </span>
+            </div>
+
 
             {favorites.length === 0 ? (
                 <p className="text-gray-600 mt-4">Please add your favorites!</p>
--- src/pages/RatingPage.tsx
+++ src/pages/RatingPage.tsx
@@ -6,11 +6,24 @@ export default function RatingPage() {
     const { state, dispatch } = useRating();
     const ratings = state.ratings;
 
+    const totalRated = ratings.filter(r => r.rating > 0);
+    const avgRating =
+        totalRated.length > 0
+            ? (totalRated.reduce((sum, r) => sum + r.rating, 0) / totalRated.length).toFixed(1)
+            : "0";
+
+
     return (
         <div className="min-h-screen bg-comic-pattern p-4 text-left mt-35 ml-18">
-            <h1 className="text-4xl font-bold text-red-600 bg-yellow-300 border-4 border-black px-4 py-2 mb-6 w-fit">
-                YOUR RATINGS
-            </h1>
+            {/* Titel + genomsnitt */}
+            <div className="flex justify-between items-center mb-6">
+                <h1 className="text-4xl font-bold text-red-600 bg-yellow-300 border-4 border-black px-4 py-2">
+                    YOUR RATINGS
+                </h1>
+                <span className="text-md text-black mr-18 font-semibold bg-pink-500 border-4 border-black shadow-[4px_4px_0px_black] p-2 text-center [backface-visibility:hidden] transition duration-300 ease-in-out hover:scale-110 hover:-translate-y-1">
+                    Average rating: {avgRating} / 5
+                </span>
+            </div>
 
             {ratings.length === 0 ? (
                 <p className="text-gray-600 mt-4">No ratings yet!</p>
